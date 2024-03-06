a2061eab6bdc390ba653aa5974a9b5494a715a20
André Rosatto <linus1178@gmail.com>
2024-03-06 11:03:04 -0300

    html inicial

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<div className="Home">
+			<header>
+				<h1>newsFinder</h1>
+				<h2>Encontre notícias de maneira fácil</h2>
+				<p>Digite o que deseja no campo de buscas e confira os resultados</p>
+			</header>
+		</div>
+	);
+}
