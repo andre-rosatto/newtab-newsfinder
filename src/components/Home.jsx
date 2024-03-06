ddad7d6a74738996357e1b88cb9df78570a88016
André Rosatto <linus1178@gmail.com>
2024-03-06 11:44:26 -0300

    html e css iniciais

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,29 @@
+import '../css/Home.css';
+
 export default function Home() {
 	return (
 		<div className="Home">
 			<header>
-				<h1>newsFinder</h1>
-				<h2>Encontre notícias de maneira fácil</h2>
-				<p>Digite o que deseja no campo de buscas e confira os resultados</p>
+				<div>
+					<h1>newsFinder</h1>
+					<h2>Encontre notícias de maneira fácil</h2>
+					<p>Digite o que deseja no campo de buscas e confira os resultados</p>
+				</div>
+				<nav>
+					<ul>
+						<li>
+							<a className="about" href="">SOBRE</a>
+						</li>
+						<li>
+							<a className="login" href="">LOGIN</a>
+						</li>
+					</ul>
+				</nav>
 			</header>
+			<form>
+				<button></button>
+				<input type="text" placeholder="Buscar" />
+			</form>
 		</div>
 	);
 }
