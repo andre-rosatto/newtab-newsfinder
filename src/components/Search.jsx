ad8e194c8bddd0bdf56c7b5f3f2f08d4d107cc75
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-03-17 17:20:37 -0300

    pages search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,34 +1,52 @@
 import React, { useState, useEffect } from "react";
 import '../css/search.css';
-// import { GetLoginsDetails } from './LoginsDetails';
 import { useNavigate } from "react-router";
 import axios from "axios";
 
 const Search = () => {
-    // const logins = GetLoginsDetails();
     const [buscas, setBuscas] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
+    const [hasMore, setHasMore] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
-        const fetchBuscas = async () => {
-            try {
-                const response = await axios.get('https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas?maxRecords=9&view=Grid%20view', {
-                    headers: {
-                        Authorization: 'Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439'
-                    }
-                });
-                setBuscas(response.data.records);
-            } catch (error) {
-                console.error('Erro ao buscar dados da API:', error);
-            }
-        };
         fetchBuscas();
-    }, []);
-    // })
+    }, [page]);
+
+const fetchBuscas = async () => {
+    setLoading(true);
+    try {
+        const response = await axios.get('https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas?maxRecords=5&view=Grid%20view', {
+            headers: {
+                Authorization: 'Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439'
+            }
+        });
+        
+        if (response.data.offset) {
+            setHasMore(true);
+        } else {
+            setHasMore(false);
+        }
+
+        setBuscas(prevBuscas => {
+            const newRecords = response.data.records.filter(record => !prevBuscas.some(busca => busca.id === record.id));
+            return [...prevBuscas, ...newRecords];
+        });
+    } catch (error) {
+        console.error('Erro ao buscar dados da API:', error);
+    } finally {
+        setLoading(false);
+    }
+};
 
     const handleLogout = () => {
         navigate('/')
     };
+
+    const handleNextPage = () => {
+        setPage(prevPage => prevPage + 1);
+    }
  
     return (
         <div className="main">
@@ -46,8 +64,7 @@ const Search = () => {
                         <span className="dot">.</span> 
                         <span onClick={handleLogout}>SAIR</span>
                         </button>
-                    </div>
-                
+                    </div> 
             </div>
                 <div>
                     <h2 className="titleBuscas">Buscas Realizadas</h2>
@@ -68,15 +85,11 @@ const Search = () => {
                                 </tr>
                             ))}
                         </tbody>
-                            {/* {logins.map((login, index) => (
-                            <tr key={index}>
-                                <td className="colBuscas">{login.username}</td>
-                                <td className="colDate">{new Date(login.loginTime).toLocaleDateString()}</td>
-                                <td className="colTime">{new Date(login.loginTime).toLocaleTimeString()}</td>
-                            </tr>
-                            ))} */}
-                        
                     </table>
+                    {loading && <p>Carregando...</p>}
+                    {hasMore && !loading && (
+                        <button onClick={handleNextPage}>Próxima pagina</button>
+                    )}
                 </div>
         </div>
     );
