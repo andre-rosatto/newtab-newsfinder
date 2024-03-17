156dcc6cfb3a4cb46a1bbc01cb72e2d35bd2c654
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-03-17 18:54:04 -0300

    API - list records

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,33 +6,51 @@ import axios from "axios";
 const Search = () => {
     const [buscas, setBuscas] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState(1);
-    const [hasMore, setHasMore] = useState(true);
+    const [offset, setOffset] = useState(null);
+    // const [page, setPage] = useState(1);
+    // const [hasMore, setHasMore] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
         fetchBuscas();
-    }, [page]);
+    }, []);
 
 const fetchBuscas = async () => {
     setLoading(true);
     try {
-        const response = await axios.get('https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas?maxRecords=5&view=Grid%20view', {
+        const response = await axios.get('https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas?maxRecords=10&pageSize=5&view=Grid%20view', {
             headers: {
                 Authorization: 'Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439'
             }
-        });
-        
-        if (response.data.offset) {
-            setHasMore(true);
-        } else {
-            setHasMore(false);
-        }
 
-        setBuscas(prevBuscas => {
-            const newRecords = response.data.records.filter(record => !prevBuscas.some(busca => busca.id === record.id));
-            return [...prevBuscas, ...newRecords];
         });
+
+        // setBuscas(prevBuscas => [...prevBuscas, ...response.data.records]);
+        setBuscas(response.data.records);
+        setOffset(response.data.offset);
+        console.log(response.data)
+
+        // const newRecords = response.data.records;
+        // console.log(newRecords)
+
+        // if (newRecords.length > 0) {
+        //     setHasMore(false);
+        // } else {
+        //     setHasMore(true);
+        // }
+
+        // setBuscas(newRecords);
+        
+        // if (response.data.offset) {
+        //     setHasMore(true);
+        // } else {
+        //     setHasMore(false);
+        // }
+
+        // setBuscas(prevBuscas => {
+        //     const newRecords = response.data.records.filter(record => !prevBuscas.some(busca => busca.id === record.id));
+        //     return [...prevBuscas, ...newRecords];
+        // });
     } catch (error) {
         console.error('Erro ao buscar dados da API:', error);
     } finally {
@@ -45,7 +63,8 @@ const fetchBuscas = async () => {
     };
 
     const handleNextPage = () => {
-        setPage(prevPage => prevPage + 1);
+        fetchBuscas();
+        // setPage(prevPage => prevPage + 1);
     }
  
     return (
@@ -87,7 +106,9 @@ const fetchBuscas = async () => {
                         </tbody>
                     </table>
                     {loading && <p>Carregando...</p>}
-                    {hasMore && !loading && (
+                </div>
+                <div>
+                {!loading && offset && (
                         <button onClick={handleNextPage}>Próxima pagina</button>
                     )}
                 </div>
