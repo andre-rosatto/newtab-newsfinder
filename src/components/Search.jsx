4c6733c0f0af6f230031ad1503ca53d80a7d9fae
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-03-17 09:35:47 -0300

    API Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../css/search.css';
-import { GetLoginsDetails } from './LoginsDetails';
+// import { GetLoginsDetails } from './LoginsDetails';
 import { useNavigate } from "react-router";
+import axios from "axios";
 
 const Search = () => {
-    const logins = GetLoginsDetails();
-
+    // const logins = GetLoginsDetails();
+    const [buscas, setBuscas] = useState([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchBuscas = async () => {
+            try {
+                const response = await axios.get('https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas?maxRecords=9&view=Grid%20view', {
+                    headers: {
+                        Authorization: 'Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439'
+                    }
+                });
+                setBuscas(response.data.records);
+            } catch (error) {
+                console.error('Erro ao buscar dados da API:', error);
+            }
+        };
+        fetchBuscas();
+    }, []);
+    // })
 
     const handleLogout = () => {
         navigate('/')
-    }
+    };
  
     return (
         <div className="main">
@@ -42,18 +60,26 @@ const Search = () => {
                             </tr>
                         </thead>
                         <tbody className="tableBody">
-                            {logins.map((login, index) => (
+                            {buscas.map((busca, index) => (
+                                <tr key={index}>
+                                    <td className="colBuscas">{busca.fields.Busca}</td>
+                                    <td className="colDate">{new Date(busca.fields.Data).toLocaleDateString()}</td>
+                                    <td className="colTime">{new Date(busca.createdTime).toLocaleTimeString()}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                            {/* {logins.map((login, index) => (
                             <tr key={index}>
                                 <td className="colBuscas">{login.username}</td>
                                 <td className="colDate">{new Date(login.loginTime).toLocaleDateString()}</td>
                                 <td className="colTime">{new Date(login.loginTime).toLocaleTimeString()}</td>
                             </tr>
-                            ))}
-                        </tbody>
+                            ))} */}
+                        
                     </table>
                 </div>
         </div>
-    )
-}
+    );
+};
 
 export default Search;
