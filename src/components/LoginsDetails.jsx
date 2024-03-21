50ad31d5249520192a8aaeb45a042f5475e3adab
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-03-20 23:32:05 -0300

    API GET

--- src/components/LoginsDetails.jsx
+++ src/components/LoginsDetails.jsx
@@ -1,9 +1,9 @@
-export const AddLoginsDetails = (setLogins, username, loginTime) => {
-    const logins = JSON.parse(localStorage.getItem('logins')) || [];
-    console.log("Dados salvos no localStorage:", logins);
-    localStorage.setItem('logins', JSON.stringify([...logins, { username, loginTime }]));
-};
+// export const AddLoginsDetails = (setLogins, username, loginTime) => {
+//     const logins = JSON.parse(localStorage.getItem('logins')) || [];
+//     console.log("Dados salvos no localStorage:", logins);
+//     localStorage.setItem('logins', JSON.stringify([...logins, { username, loginTime }]));
+// };
 
-export const GetLoginsDetails = () => {
-    return JSON.parse(localStorage.getItem('logins')) || [];
-}
+// export const GetLoginsDetails = () => {
+//     return JSON.parse(localStorage.getItem('logins')) || [];
+// }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,64 +7,132 @@ const Search = () => {
     const [buscas, setBuscas] = useState([]);
     const [loading, setLoading] = useState(false);
     const [offset, setOffset] = useState(null);
-    // const [page, setPage] = useState(1);
-    // const [hasMore, setHasMore] = useState(true);
+    const [semMaisItens, setSemMaisItens] = useState(false);
+    const [page, setPage] = useState(1);
+    const pageSize = 10;
     const navigate = useNavigate();
 
     useEffect(() => {
+        const fetchBuscas = async () => {
+            setLoading(true);
+            try {
+                const myHeaders = new Headers();
+                myHeaders.append("Authorization", "Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439")
+            
+
+                let url = "https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas";
+
+                if (offset) {
+                    url += '&offset=' + offset;
+                }
+
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers: myHeaders
+                });
+
+                const result = await response.json();
+
+                if(result.offset) {
+                    setOffset(result.offset);
+                } else {
+                    setSemMaisItens(true)
+                }
+
+                setBuscas(result.records);
+                console.log(result.records);
+            } catch (error) {
+                console.log("Erro", error)
+            } finally {
+                setLoading(false)
+            };
+        }
+
         fetchBuscas();
-    }, []);
+    }, [page]); 
 
-const fetchBuscas = async () => {
-    setLoading(true);
-    try {
-        const response = await axios.get('https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas?maxRecords=10&pageSize=5&view=Grid%20view', {
-            headers: {
-                Authorization: 'Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439'
-            }
+    // var offset = "";
+    // var semMaisItens = "";
 
-        });
+    // function getNextPage() {
+    //     if (!semMaisItens) {
+    //         const myHeaders = new Headers();
+    //         myHeaders.append("Authorization", "Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439")
 
-        // setBuscas(prevBuscas => [...prevBuscas, ...response.data.records]);
-        setBuscas(response.data.records);
-        setOffset(response.data.offset);
-        console.log(response.data)
+    //         let url = "https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas";
 
-        // const newRecords = response.data.records;
-        // console.log(newRecords)
+    //         if (offset) {
+    //             url += '&offset=' + offset;
+    //         }
 
-        // if (newRecords.length > 0) {
-        //     setHasMore(false);
-        // } else {
-        //     setHasMore(true);
-        // }
+    //         fetch(url, {
+    //             method: "GET",
+    //             headers: myHeaders
+    //         })
 
-        // setBuscas(newRecords);
+    //         .then((response) => response.json())
+    //         .then((result) => {
+    //             if(result && result.offset) {
+    //                 offset = result.offset;
+    //             } else {
+    //                 semMaisItens = true;
+    //             }
+
+    //             console.log(result.records);
+    //         })
+
+    //         .catch((error) => console.error(error));
+    //     } else {
+    //         console.log("Sem mais dados")
+    //     }
+
+    //     getNextPage()
+    // }
+
+
+
+// useEffect(() => {
+//     const fetchBuscas = async () => {
+//         setLoading(true);
+//         try {
+//             const response = await axios.get('https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas', {
+//                 params: {
+//                     maxRecords: pageSize,
+//                     pageSize: pageSize,
+//                     view: 'Grid view',
+//                     offset: (page - 1) * pageSize,
+//                 },
+//                 headers: {
+//                     Authorization: 'Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439'
+//                 }
+    
+//             });
+    
+//             setBuscas(response.data.records);
+//             setOffset(response.data.offset);
+//             console.log(response.data.offset)
         
-        // if (response.data.offset) {
-        //     setHasMore(true);
-        // } else {
-        //     setHasMore(false);
-        // }
+//         } catch (error) {
+//             console.error('Erro ao buscar dados da API:', error);
+//         } finally {
+//             setLoading(false);
+//         }
+//     };
 
-        // setBuscas(prevBuscas => {
-        //     const newRecords = response.data.records.filter(record => !prevBuscas.some(busca => busca.id === record.id));
-        //     return [...prevBuscas, ...newRecords];
-        // });
-    } catch (error) {
-        console.error('Erro ao buscar dados da API:', error);
-    } finally {
-        setLoading(false);
-    }
-};
+//     fetchBuscas();
+// }, [page]);
+
 
     const handleLogout = () => {
         navigate('/')
     };
 
     const handleNextPage = () => {
-        fetchBuscas();
-        // setPage(prevPage => prevPage + 1);
+        setPage(prevPage => prevPage + 1);
+    }
+
+    const handlePreviousPage = () => {
+        setPage(prevPage => Math.max(prevPage - 1, 1));
     }
  
     return (
@@ -108,8 +176,11 @@ const fetchBuscas = async () => {
                     {loading && <p>Carregando...</p>}
                 </div>
                 <div>
-                {!loading && offset && (
+                {!loading && offset && buscas.length === pageSize && (
+                    <>
+                        <button onClick={handlePreviousPage} disabled={page === 1}>Página Anterior</button>
                         <button onClick={handleNextPage}>Próxima pagina</button>
+                    </>
                     )}
                 </div>
         </div>
