c7873cf2f2c6c4f3d6ba2c8ee418a77cb8f86e17
Vinicyos Ferreira <vinifr987@gmail.com>
2024-03-23 10:37:59 -0300

    Busca de dados na API

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
 import styles from './Main.module.css'
 import { FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa";
 import Ilustration from '../imgs/about-illustration.svg'
 import MemberPhoto3 from '../imgs/img-projeto.jpeg'
+import {useFetch}  from '../hooks/useFetch';
 
 function MainAbout() {
+  const url = `https://api.airtable.com/v0/app18hif6rR0tVAkT/`;
+  const {dataDescription : description , dataEquip : equip} = useFetch(url);
+  
   return (
-    <section className={styles.main}>
-        
+    <section className={styles.main}>    
         <section className={styles.description}>
             <h3>O que é</h3>
-            <p>O NewsFinder é um projeto voltado para localização de noticias , onde é usado requisições 
-               á API(Airtable) para buscar e inserir dados , as tecnologias usadas foram Javascript , 
-               React , HTML e CSS para implementar as páginas correspondente com salvamento e listagem
+            <p>O NewsFinder é um projeto voltado para localização de noticias, onde é usado requisições 
+               á API(Airtable) para buscar e inserir dados, as tecnologias usadas foram Javascript , 
+               React, HTML e CSS para implementar as páginas correspondente com salvamento e listagem
                de informações 
             </p>
+            {description.map((value) => <p key={'index' + value.id}>{value.fields.Sobre}</p>)}
         </section>
 
         <section className={styles.ilustration}>
@@ -26,9 +29,10 @@ function MainAbout() {
         <section className={styles.whoWeAre}>
               <h3>Quem somos</h3>
                <section className={styles.teamBio}> 
+
                   <div className={styles.members}>
                       <img/>
-                      <p>Nome Sobrenome</p>
+                      {equip.map((value) => <p>{value.fields.Nome}</p>)}
                       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
                         Unde, eum beatae velit enim porro tenetur officiis ipsam earum nulla expedita. 
                         Doloribus mollitia saepe recusandae veniam sequi sunt impedit sit nihil.
@@ -63,7 +67,7 @@ function MainAbout() {
                        conhecimentos em programação front end e sistema ERP
                       </p>
                       <div className={styles.socialMedia}>
-                           <a href='https://github.com/VinicyosFerreira' target='blank'><FaGithub/></a>
+                          <a href='https://github.com/VinicyosFerreira' target='blank'><FaGithub/></a>
                           <a href='https://mailto:vinifr987@gmail.com'  target='blank'><MdEmail/></a>
                           <a href='https://www.linkedin.com/in/vinicyos-ferreira/' target='blank'><FaLinkedin/></a>
                       </div>
---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,48 @@
+import { useState , useEffect } from 'react'
+
+export const useFetch = (url) => {
+    const [dataDescription , setDataDescription] = useState([]);
+    const [dataEquip , setDataEquip] = useState([]);
+    const [table , setTable] = useState('Projeto');
+    const token = 'patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439';
+
+    const parameter = {
+        filterByFormula : "({Squad} = '03-24')"
+    }
+
+    const encodedParameter = Object.keys(parameter).map((key) => {
+        return key+'='+encodeURI(parameter[key]);
+    });
+    
+    useEffect(() => {
+        async function fetchData () {
+            try{
+                const response = await fetch(`${url}${table}?${encodedParameter}&`, {
+                    headers: {
+                        Authorization:'Bearer ' + token
+                    }
+                })
+                const dataResponse = await response.json();
+                console.log(dataResponse);
+
+                if(table === 'Projeto') {
+                    setDataDescription(dataResponse.records);
+                } else {
+                    setDataEquip(dataResponse.records)
+                }
+    
+                setTable('Equipe');
+            }
+             catch(error) {
+                console.log(error);
+            }
+        }
+        fetchData();
+        
+    } , [table , url])
+
+  return {dataDescription , dataEquip  , table}
+
+}
+
+
