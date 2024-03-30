f8f724c0ac0a1397cd88a30357986817e4a3859f
Lara Gabriela <lara_g_lopes@hotmail.com>
2024-03-30 13:59:36 -0300

    ajustes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -16,98 +16,57 @@ function Search() {
                     null,
                     data => {
                         setSearchResults(data.records);
-                        console.log(data.records)
                         if (data.offset) {
                             setOffsets([data.offset]);
+                        }
+
+                        if (data.records.length === 0) {
+                            setSemMaisItens(true);
+                            console.log('semMaisItens atualizado:', semMaisItens);
+                           
                         }
                     },
                     error => console.error(error)
                 );
-            } else if (currentPage > 0) {
+            } else if (currentPage > 0 && offsets[currentPage -1]) {
                 // Páginas subsequentes
+                
                 AirtableHandler.getSearches(
                     offsets[currentPage - 1],
-                data => {
-                    if (data.records.length > 0) {
-                        setSearchResults(data.records);
-                        setOffsets(prevOffsets => [...prevOffsets, data.offset]);
-                    } else {
-                        setSemMaisItens(true);
-                    }
-                },
-                error => console.error(error)
-            );
+                    data => {
+                        if (data.records.length > 0) {
+                            setSearchResults(data.records);
+                            setOffsets(prevOffsets => [...prevOffsets, data.offset]);
+                        } else {
+                            if (data.records.length === 0) {
+                                console.log(data.records.length);
+                                setSemMaisItens(true);
+                                console.log('semMaisItens atualizado:', semMaisItens);
+                            }
+                        }
+                    },
+                    error => console.error(error)
+                );
+            }
+        } else {
+            console.log('Fim dos dados');
         }
-    } else {
-        setSemMaisItens(true);
-        console.log('Fim dos dados');
-    }
-};
-
-                    //     setSearchResults(data.records);
-                    //     console.log(data.records)
-                    //     if (data.offset) {
-                    //         setOffsets(prevOffsets => [...prevOffsets, data.offset]);
-                    //     } else {
-                    //         setSemMaisItens(true);
-                    //     }
-                    // },
-                    
-
-
-    //Buscar a próxima página com os dados da API
-    // const getNextPage = () => {
-    //     if (!semMaisItens) {
-    //         const myHeaders = new Headers();
-    //         myHeaders.append("Authorization", "Bearer patFWS9nyevnpN89P.981364c0cc345536e73139edfae790d9727211ee50e99f1f8af8fe867467f439");
-
-    //         let url = 'https://api.airtable.com/v0/app18hif6rR0tVAkT/Buscas?pageSize=10&view=Grid%20view';
-
-    //         if (currentPage > 0) {
-    //             url += `&offset=${offsets[currentPage - 1]}`;
-    //         }
-
-    //         fetch(url, {
-    //             method: "GET",
-    //             headers: myHeaders
-    //         })
-    //         .then((response) => response.json())
-    //         .then((result) => {
-    //             if (result && result.offset) {
-    //                 setOffsets([...offsets, result.offset]); // Armazenar o novo offset
-    //             } else {
-    //                 setSemMaisItens(true);
-    //             }
-
-    //             setSearchResults(result.records);
-    //         })
-    //         .catch((error) => console.log(error));
-    //     } else {
-    //         console.log('Fim dos dados');
-    //     }
-    // };
+    };
 
     //Ir para a próxima página
     const handleNextPage = () => {
-        setCurrentPage(currentPage + 1);
+        if (!semMaisItens && currentPage < offsets.length) {
+            setCurrentPage(currentPage + 1);
+        }
     };
 
+    //Voltar a página anterior
     const handlePreviousPage = () => {
         if (currentPage > 0) {
             setCurrentPage(currentPage - 1);
             getNextPage();
         }
     };
-
-    // const handleFirstPage = () => {
-    //     setCurrentPage(0);
-    //     setSemMaisItens(false); // Reinicia semMaisItens para false ao voltar para a primeira página
-    //     getNextPage();
-    // }
-
-    // const handleLastPage = () => {
-    //     setCurrentPage(offsets.length - 1);
-    // };
 
     useEffect(() => {
         getNextPage();
@@ -137,7 +96,7 @@ function Search() {
                 </table>
                 <div className="btPages">
                     <button onClick={handlePreviousPage} disabled={currentPage === 0}>&#60;</button>
-                    <button onClick={handleNextPage} disabled={semMaisItens}>&#62;</button>
+                    <button onClick={handleNextPage} disabled={semMaisItens === true}>&#62;</button>
                 </div>
             </div>
             
@@ -146,4 +105,3 @@ function Search() {
 }
 
 export default Search;
- 
